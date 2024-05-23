d9c295e33b9c197b5a0ce850328d040daf2c4fe4
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2024-05-23 19:00:29 +0100

    8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,1 +1,8 @@
-
+-- lists all cities of california that can be found in the database
+-- where the states table contain only one record where name = california
+-- results are sorted in ASC by cities.id
+SELECT id, name FROM cities WHERE state_id = (
+  SELECT id
+  FROM states
+  WHERE name='California'
+) ORDER BY cities.id ASC;
