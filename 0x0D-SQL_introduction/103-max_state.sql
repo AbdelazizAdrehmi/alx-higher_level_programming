5ea450aaf596a0d44d48f2997882e6e38ecfcede
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2024-05-23 18:30:45 +0100

    103-max_state.sql

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,1 +1,6 @@
-
+-- displays the max temprature of each state ordered by ststename
+-- source temperatures.sql;
+SELECT state, MAX(value) AS max_temp
+FROM temperatures
+GROUP BY state
+ORDER BY state ASC;
