72e8b6b8bf1dae3116994eaca38161dde3cbd4d5
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2024-06-13 22:19:55 +0100

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """ Add all arguments."""
+    import sys
 
+    result = 0
+
+    for i in range(len(sys.argv) - 1):
+        result += (int(sys.argv[i + 1]))
+    print("{:d}".format(result))
