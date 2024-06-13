c4c4eb99860bc1f9653e5b685b1b24e0c0f52983
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2024-06-13 23:31:59 +0100

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -8,7 +8,10 @@ def magic_calculation(a, b):
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return (c)
+        return (c) 
+/n
 
     else:
         return(sub(a, b))
+/n
+
