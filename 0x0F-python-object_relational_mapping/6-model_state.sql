c9d488b4713a6c1963524df20dadca98fed6c5a6
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2024-06-24 00:00:42 +0100

    6-model_state.sql

--- 0x0F-python-object_relational_mapping/6-model_state.sql
+++ 0x0F-python-object_relational_mapping/6-model_state.sql
@@ -1,11 +1,4 @@
-#!/usr/bin/python3
-"""Start link class to table in database
-"""
-import sys
-from model_state import Base, State
-
-from sqlalchemy import (create_engine)
-
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+-- Create database hbtn_0e_6_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0e_6_usa;
+USE hbtn_0e_6_usa;
+SHOW CREATE TABLE states;
