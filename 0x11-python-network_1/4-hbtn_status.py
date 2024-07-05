27ab6fdc06ec5e6ecda3b76d6ec99f63ca9fcde0
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2024-07-05 19:32:28 +0100

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""
-Same as Task 0 but using request package
-"""
-
+"""fetches https://intranet.hbtn.io/status."""
 import requests
 
 
 if __name__ == "__main__":
-    response = requests.get('https://intranet.hbtn.io/status')
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
